defae5e45c914e395e948531ba77ad564b7d057c
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-03 22:48:14 +0900

    Ranking controller 추가

---
+++ src/main/java/com/emirim/gotoschool/controller/RankingController.java
@@ -0,0 +1,35 @@
+package com.emirim.gotoschool.controller;
+
+import com.emirim.gotoschool.domain.Ranking;
+import com.emirim.gotoschool.dto.AddRankingRequest;
+import com.emirim.gotoschool.service.RankingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ranking")
+public class RankingController {
+
+    private final RankingService rankingService;
+
+    @Autowired
+    public RankingController(RankingService rankingService) {
+        this.rankingService = rankingService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ranking>> findAll() {
+        return ResponseEntity.status(200)
+                .body(rankingService.findAllRanking());
+    }
+
+    @PostMapping
+    public ResponseEntity<Ranking> createRanking(@RequestBody AddRankingRequest addRankingRequest) {
+        return ResponseEntity.status(201)
+                .body(rankingService.createRanking(addRankingRequest));
+    }
+
+}
