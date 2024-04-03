65893aab43d26edc966240816d1ee17e5054b4dc
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-03 23:05:57 +0900

    Cors 설정 추가

--- src/main/java/com/emirim/gotoschool/controller/RankingController.java
+++ src/main/java/com/emirim/gotoschool/controller/RankingController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/ranking")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class RankingController {
 
     private final RankingService rankingService;
