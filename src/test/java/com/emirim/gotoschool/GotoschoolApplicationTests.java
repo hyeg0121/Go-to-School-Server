df1037cbaa6a0d897824bb3bca3204c9849fffbb
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-03 22:47:06 +0900

    init

---
+++ src/test/java/com/emirim/gotoschool/GotoschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirim.gotoschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotoschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
